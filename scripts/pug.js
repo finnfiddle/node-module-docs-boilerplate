77b615ac7abda38afdc182a0500ed811e249f675
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2016-11-08 21:50:48 +0200

    v1

--- scripts/pug.js
+++ scripts/pug.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash')
 const pug = require('pug');
 const fs = require('fs');
 const path = require('path');
@@ -7,14 +8,25 @@ const npm = require('machinepack-npm');
 
 const config = require('../config.json');
 
+console.log('compiling pug...');
+
 npm.fetchInfo({ packageName: config.packageName }).exec({
   success: function (npmConfig){
-    const template = pug.compileFile(path.join(__dirname, '../src/index.pug'));
+    const template = pug.compileFile(
+      path.join(__dirname, '../src/index.pug'),
+      {
+        pretty: true,
+        filters: {
+          escape: _.escape,
+        },
+      }
+    );
 
     fs.writeFileSync(
       path.join(__dirname, '../index.html'),
       template(Object.assign({}, npmConfig, config))
     );
+    console.log('compiled pug...');
   },
   // An unexpected error occurred.
   error: function (err){
